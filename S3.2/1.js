df60f6f67dfc60aa262758a61ebabbb74f840255
ulis891 <99750497+ulis891@users.noreply.github.com>
2023-07-17 11:55:35 +0000

    add S3.2

---
+++ S3.2/1.js
@@ -0,0 +1,3 @@
+let temp_c = prompt('Введите теипературу в градусах Цельсия')
+let temp_f = ((9 / 5) * temp_c + 32).toFixed(1)
+alert('Цельсий: ' +  temp_c + ' ' + 'Фарингейт: ' + temp_f)
---
+++ S3.2/2.js
@@ -0,0 +1,8 @@
+function greeting(a)
+        {
+            console.log('Привет,' + ' ' + a + '!') 
+        }
+
+let name = prompt('Введите ваше имя.', 'Имя')
+
+greeting(name)
